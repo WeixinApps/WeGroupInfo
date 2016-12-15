f8dc81a7452e3f65ddc7905660a35c8a5bfb4e9f
zdt@macbook <dev@dthappy.com>
2016-12-15 21:14:02 +0800

    add index.js

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+class Index{
+    constructor(){
+        this.app = getApp();
+        this.data = {
+            
+        }
+        this.bindMethods();
+    }
+    bindMethods(){
+
+    }
+    bindViewTap(){
+
+    }
+    onLoad(){
+
+    }
+}
+Page(new Index());
