3438982330804981497238b46550080155fb2626
ZDT@VAIO <dev@dthappy.com>
2017-01-08 23:43:20 +0800

    add share event

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,5 +15,13 @@ class Index{
     onLoad(){
 
     }
+    onShareAppMessage(){
+        return {
+        title: '广告板 广而告之 有你所需',
+        desc: '微信群通知、问卷、公告等信息助手',
+        path: '/page/index?id=123'
+        }
+    }
+
 }
 Page(new Index());
