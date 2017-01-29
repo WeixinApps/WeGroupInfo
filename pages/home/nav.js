9d9f26c13ce6095c9e9571f1a11f233195def745
ZDT@HP <dev@dthappy.com>
2017-01-29 19:08:22 +0800

    add nav template

---
+++ pages/home/nav.js
@@ -0,0 +1,27 @@
+const navItems =  {
+    "items": [
+        {
+            "url": "index",
+            "text": "热点"
+        }, {
+            "url": "food",
+            "text": "美食"
+        }, {
+            "url": "shop",
+            "text": "购物"
+        }, {
+            "url": "travel",
+            "text": "旅行"
+        }, {
+            "url": "beauty",
+            "text": "美丽"
+        }, {
+            "url": "life",
+            "text": "生活"
+        }, {
+            "url": "game",
+            "text": "游戏"
+        }
+    ]
+};
+module.exports.navItems = navItems; 
