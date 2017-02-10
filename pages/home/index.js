61e929e54f1219f441f84f5732a7c6e42cbae223
ZDT@VAIO <dev@dthappy.com>
2017-02-10 11:33:56 +0800

    update nav

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,19 +1,23 @@
 // const nav = require('nav.js');
 import nav from 'nav';
+
 class Index{
     constructor(){
         this.app = getApp();
         this.data = {nav,current:"index"};
         this.bindMethods();
+        console.log(this) 
     }
     bindMethods(){
        this.bindNavTap = this.bindNavTap.bind(this);
+       //this.onLoad = this.onLoad.bind(this);
     }
     bindViewTap(){
 
     }
     onLoad(options){
-        console.log(options);
+        //this is overwrited
+        //console.log(options);
         console.log(options.query);
         console.log(this)
         this.setData({
@@ -26,7 +30,7 @@ class Index{
         this.setCurrent(e.target.id);
     }
     setCurrent(current){
-        this.setData({
+        this.page.setData({
           current: current
         });
     }
--- pages/location/index.js
+++ pages/location/index.js
@@ -1,27 +1,10 @@
-class Index{
-    constructor(){
-        this.app = getApp();
-        this.data = {
-            
-        }
-        this.bindMethods();
-    }
-    bindMethods(){
-
-    }
-    bindViewTap(){
-
-    }
-    onLoad(){
-
-    }
-    onShareAppMessage(){
-        return {
-        title: '广告板 广而告之 有你所需',
-        desc: '微信群通知、问卷、公告等信息助手',
-        path: '/page/index?id=123'
-        }
-    }
-
-}
-Page(new Index());
+const page = {
+  data: {
+    text: "This is page data."
+  },
+  onLoad(options){
+    // Do some initialize when page load.
+    console.log(this);
+  }
+};
+Page(page);
