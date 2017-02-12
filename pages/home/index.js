61f684faa51fbd157e3d27d66c2218346f81df6b
ZDT@HP <dev@dthappy.com>
2017-02-12 12:37:34 +0800

    add content template

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,7 +5,7 @@ let _this;
 class Index{
     constructor(){
         this.app = getApp();
-        this.data = {nav,current:"index"};
+        this.data = {nav,current:"index",content:{items:[]}};
         this.bindMethods();
     }
     bindMethods(){
@@ -25,6 +25,36 @@ class Index{
     }
     bindNavTap(e){
         this.setCurrent(e.target.id);
+        this.setPageContent(e.target.id);
+    }
+    getPageContent(page){
+        return {items:[
+            {
+                image:"http://youyouplay.com/public/images/banner-3.jpg",
+                title: page+"去日本旅行最值得买的100件化妆品",
+                summary:"最美生活从旅行开始，现在出发！",
+                info:"跟着企鹅去韩国",
+                time:"3小时前"
+            }, {
+                image:"http://youyouplay.com/public/images/banner-3.jpg",
+                title:"去日本旅行最值得买的100件化妆品",
+                summary:"最美生活从旅行开始，现在出发！",
+                info:"跟着企鹅去韩国",
+                time:"5小时前"
+            }, {
+                image:"http://youyouplay.com/public/images/banner-3.jpg",
+                title:"去日本旅行最值得买的100件化妆品",
+                summary:"最美生活从旅行开始，现在出发！",
+                info:"跟着企鹅去韩国",
+                time:"9小时前"
+            }
+        ]}; 
+    }
+    setPageContent(current){
+        let items = this.getPageContent(current);
+        _this.setData({
+            content:items
+        })
     }
     setCurrent(current){
         _this.setData({
