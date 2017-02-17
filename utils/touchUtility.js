1529f6868d38b6a063174f0d89c82fff743cd164
ZDT@HP <dev@dthappy.com>
2017-02-17 12:44:36 +0800

    update touchUtility

--- utils/touchUtility.js
+++ utils/touchUtility.js
@@ -23,21 +23,20 @@ class TouchUtility{
         eventsUtility.isSlideRight = false;
     }
     bindTouchMove(e){ 
-       if((eventsUtility.touchStartX-e.touches[0].pageX)>eventsUtility.slideDistance
-            && Math.abs(eventsUtility.touchStartY-e.touches[0].pageY)<eventsUtility.slideDistance/2){
+       if((eventsUtility.touchStartX-e.touches[0].pageX)>eventsUtility.slideDistance){
             eventsUtility.isSlideLeft = true;
         }else{
             eventsUtility.isSlideLeft = false; 
         }
-        if(e.touches[0].pageX-eventsUtility.touchStartX>eventsUtility.slideDistance 
-           && Math.abs(eventsUtility.touchStartY-e.touches[0].pageY)<eventsUtility.slideDistance/2){
+        if(e.touches[0].pageX-eventsUtility.touchStartX>eventsUtility.slideDistance){
             eventsUtility.isSlideRight = true;
         }else{
             eventsUtility.isSlideRight = false; 
         }
     }
     bindTouchEnd(e){
-        if(e.timeStamp-eventsUtility.touchTimeStamp>eventsUtility.slideDuration) return;
+        if(e.timeStamp-eventsUtility.touchTimeStamp>eventsUtility.slideDuration 
+        || Math.abs(eventsUtility.touchStartY-e.touches[0].pageY)>eventsUtility.slideDistance/2) return;
         if(eventsUtility.isSlideLeft) this.slideLeft ? this.slideLeft() : eventsUtility.slideLeft();
         if(eventsUtility.isSlideRight) this.slideRight ? this.slideRight() : eventsUtility.slideRight();        
     }
