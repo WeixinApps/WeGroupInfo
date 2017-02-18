0d6793cf7cd927c4eacb673faa7e9681f0e32af0
zdt@macbook <dev@dthappy.com>
2017-02-18 23:36:15 +0800

    update page style and add related news

--- pages/page/page.js
+++ pages/page/page.js
@@ -1,9 +1,15 @@
-let content ={
+const content ={
     data:{
         id : 123
     },
     onLoad(options){
         this.setData({id:options.id});
+    },
+    onShareAppMessage(){
+        return {
+        title: '购物美文',
+        path: 'pages/page/page?id=123'
+        }
     }
 }
 Page(content);
